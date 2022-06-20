56eace0245e9d2f22e09ebbe91feeaf4b693bc17
Sunlly0 <54405269+Sunlly0@users.noreply.github.com>
2022-06-20 22:03:34 +0800

    2022.6.20 add code

---
+++ src/cs245/as3/LogRecord.java
@@ -0,0 +1,99 @@
+package cs245.as3;
+
+import java.nio.ByteBuffer;
+
+/**
+ * You will implement this class.
+ * <p>
+ * The implementation we have provided below performs atomic transactions but the changes are not durable.
+ * Feel free to replace any of the data structures in your implementation, though the instructor solution includes
+ * the same data structures (with additional fields) and uses the same strategy of buffering writes until commit.
+ * <p>
+ * Your implementation need not be threadsafe, i.e. no methods of TransactionManager are ever called concurrently.
+ * <p>
+ * You can assume that the constructor and initAndRecover() are both called before any of the other methods.
+ */
+public class LogRecord {
+    // 头部长度
+//		public static final int headLen = 13;
+//		// LogManager最大的byte数组长度
+//		public static final int maxLen = 128;
+
+    //		private int size;
+    //type
+    // write=0
+    // commit=1
+    // abort=2
+    public static int WRITE = 0; //write
+    public static int COMMIT = 1; // commit record
+    public static int ABORT = 2; // commit record
+    private int type;
+    private long txnId;
+    private long key;
+    private byte[] value;
+
+    private int size;
+    private int offset;
+
+    public LogRecord(long txnId, long key, byte[] value) {
+        this.txnId = txnId;
+        this.key = key;
+        this.value = value;
+    }
+
+    public LogRecord(int type, long txnId, long key, byte[] value) {
+        this.type = type;
+        this.txnId = txnId;
+        this.key = key;
+        this.value = value;
+    }
+
+    public byte[] getValue() {
+        return this.value;
+    }
+    public long getTxnId() {
+        return this.txnId;
+    }
+    public long getKey() {
+        return this.key;
+    }
+    public int getSize() {
+        return this.size;
+    }
+
+    public int getType() {
+        return this.type;
+    }
+    public int getOffset() {
+        return this.offset;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+    public void setOffset(int offset) {
+        this.offset=offset;
+    }
+
+    public static byte[] encode(LogRecord l) {
+        int size = 24 + l.value.length; // OPS
+        l.setSize(size);
+        ByteBuffer allocate = ByteBuffer.allocate(size);
+        allocate.putInt(l.size)
+                .putLong(l.txnId)
+                .putLong(l.key)
+                .put(l.value);
+        return allocate.array();
+    }
+
+    public static LogRecord decode(byte[] bytes) {
+        ByteBuffer wrap = ByteBuffer.wrap(bytes);
+        int size = wrap.getInt();
+        long tnxId = wrap.getLong();
+        long key = wrap.getLong();
+//		int state = wrap.getInt();
+        byte[] values = new byte[size - wrap.position()];
+        wrap.get(values);
+        return new LogRecord(size, tnxId, key, values);
+    }
+}
--- src/cs245/as3/TransactionManager.java
+++ src/cs245/as3/TransactionManager.java
@@ -1,23 +1,16 @@
 package cs245.as3;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
 import cs245.as3.interfaces.LogManager;
 import cs245.as3.interfaces.StorageManager;
 import cs245.as3.interfaces.StorageManager.TaggedValue;
 
-/**
- * You will implement this class.
- *
- * The implementation we have provided below performs atomic transactions but the changes are not durable.
- * Feel free to replace any of the data structures in your implementation, though the instructor solution includes
- * the same data structures (with additional fields) and uses the same strategy of buffering writes until commit.
- *
- * Your implementation need not be threadsafe, i.e. no methods of TransactionManager are ever called concurrently.
- *
- * You can assume that the constructor and initAndRecover() are both called before any of the other methods.
- */
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.PriorityQueue;
+
+
 public class TransactionManager {
 	class WritesetEntry {
 		public long key;
@@ -35,11 +28,23 @@ public class TransactionManager {
 	  * Hold on to writesets until commit.
 	  */
 	private HashMap<Long, ArrayList<WritesetEntry>> writesets;
+	// add struct by Sunlly
+	private LogManager lm;
+	private StorageManager sm;
+	private PriorityQueue<Long> offsetQueue;
+
+	//add method by Sunlly
+
+	private HashMap<Long, ArrayList<LogRecord>> logRecordSets;
 
 	public TransactionManager() {
 		writesets = new HashMap<>();
 		//see initAndRecover
 		latestValues = null;
+
+		//add by Sunlly
+		logRecordSets= new HashMap<>();
+
 	}
 
 	/**
@@ -47,7 +52,47 @@ public class TransactionManager {
 	 * At this time you should detect whether the StorageManager is inconsistent and recover it.
 	 */
 	public void initAndRecover(StorageManager sm, LogManager lm) {
+		//初始化
 		latestValues = sm.readStoredTable();
+		this.lm=lm;
+		this.sm=sm;
+
+		ArrayList<LogRecord> records = new ArrayList<>();
+		HashSet<Long> committedTxn = new HashSet<>();
+
+		//从 logManager 中获取日志，读取日志记录
+		for (int offset = lm.getLogTruncationOffset(); offset < lm.getLogEndOffset(); ) {
+			byte[] sizeBytes = lm.readLogRecord(offset, 4);
+			int size = ByteBuffer.wrap(sizeBytes).getInt();
+
+			//解析日志，从 byte到结构体
+			byte[] decodeRecode = lm.readLogRecord(offset, size);
+			LogRecord logRecord = LogRecord.decode(decodeRecode);
+			logRecord.setOffset(offset);
+
+			//保存records
+			records.add(logRecord);
+
+			//保存已经提交的 txnId, 后续做 redo
+			if (LogRecord.COMMIT == logRecord.getType()) {
+				committedTxn.add(logRecord.getTxnId());
+			}
+			offset += logRecord.getSize();
+		}
+
+		// redo 已经提交的日志(恢复)
+		for (LogRecord record : records) {
+			long txnId = record.getTxnId();
+			//遍历 records，如果在已经提交的事务列表中，并且是写操作，则做 redo
+			if (committedTxn.contains(txnId) && record.getType() == LogRecord.WRITE ) {
+				int tag = record.getOffset();
+//				offsetQueue.offer(tag);
+				// 应用日志
+				sm.queueWrite(record.getKey(), tag, record.getValue());
+				//保存最新值
+				latestValues.put(record.getKey(), new TaggedValue(tag, record.getValue()));
+			}
+		}
 	}
 
 	/**
@@ -55,12 +100,15 @@ public class TransactionManager {
 	 */
 	public void start(long txID) {
 		// TODO: Not implemented for non-durable transactions, you should implement this
+		writesets.put(txID,new ArrayList<>());
+		logRecordSets.put(txID,new ArrayList<>());
 	}
 
 	/**
 	 * Returns the latest committed value for a key by any transaction.
 	 */
 	public byte[] read(long txID, long key) {
+		// 获取 key 的最新版本的值
 		TaggedValue taggedValue = latestValues.get(key);
 		return taggedValue == null ? null : taggedValue.value;
 	}
@@ -71,22 +119,42 @@ public class TransactionManager {
 	 * to this same key from txID itself after we make a write to the key. 
 	 */
 	public void write(long txID, long key, byte[] value) {
+		// by Sunlly
+		//写 key-value，此时的写并没有被应用，而是保存到事务的写集中，等待commit时才真正被应用
+		//同时通过日志将操作记录下来
+		ArrayList<LogRecord> logRecordSet = logRecordSets.get(txID);
+		logRecordSet.add(new LogRecord(txID, key, value));
+		logRecordSets.put(txID, logRecordSet);
+
 		ArrayList<WritesetEntry> writeset = writesets.get(txID);
-		if (writeset == null) {
-			writeset = new ArrayList<>();
-			writesets.put(txID, writeset);
-		}
 		writeset.add(new WritesetEntry(key, value));
+		writesets.put(txID, writeset);
 	}
 	/**
 	 * Commits a transaction, and makes its writes visible to subsequent read operations.\
 	 */
 	public void commit(long txID) {
+		//持久化日志
+		LogRecord commitRecord = new LogRecord(LogRecord.COMMIT,txID, -1, new byte[]{});
+		ArrayList<LogRecord> logRecordSet = logRecordSets.get(txID);
+		logRecordSet.add(commitRecord);
+
+		for (LogRecord logRecord : logRecordSet) {
+			byte[] encodeRecord = logRecord.encode(logRecord);
+			lm.appendLogRecord(encodeRecord);
+//			keyToOffset.put(logRecord.getKey(), offset);
+		}
+
+		logRecordSets.put(txID, logRecordSet);
+
+		//持久化
 		ArrayList<WritesetEntry> writeset = writesets.get(txID);
 		if (writeset != null) {
 			for(WritesetEntry x : writeset) {
 				//tag is unused in this implementation:
 				long tag = 0;
+				// queueWrite 写入
+				sm.queueWrite(x.key, tag, x.value);
 				latestValues.put(x.key, new TaggedValue(tag, x.value));
 			}
 			writesets.remove(txID);
@@ -97,6 +165,7 @@ public class TransactionManager {
 	 */
 	public void abort(long txID) {
 		writesets.remove(txID);
+		logRecordSets.remove(txID);
 	}
 
 	/**
@@ -104,5 +173,9 @@ public class TransactionManager {
 	 * These calls are in order of writes to a key and will occur once for every such queued write, unless a crash occurs.
 	 */
 	public void writePersisted(long key, long persisted_tag, byte[] persisted_value) {
+		if (persisted_tag == offsetQueue.peek()) {
+			lm.setLogTruncationOffset((int)persisted_tag);
+		}
+		offsetQueue.remove(persisted_tag);
 	}
 }
